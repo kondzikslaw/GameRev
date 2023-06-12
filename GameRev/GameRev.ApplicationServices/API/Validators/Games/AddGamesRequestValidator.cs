4899b15a3da0323927a05f8d06ed1d7113d9d6ae
kondzikslaw <kondzikslaw@gmail.com>
2023-06-12 23:06:29 +0200

    Revert "Revert "Validator changes""
    This reverts commit 4d61bd3ba5e5869c11f21656b6811e2dcee32d77.

---
+++ GameRev/GameRev.ApplicationServices/API/Validators/Games/AddGamesRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using GameRev.ApplicationServices.API.Domain.Requests.Games;
+
+namespace GameRev.ApplicationServices.API.Validators.Games
+{
+    public class AddGamesRequestValidator : AbstractValidator<AddGamesRequest>
+    {
+        public AddGamesRequestValidator()
+        {
+            RuleFor(x => x.Title).Length(1, 300).WithMessage("Incorrect value length. Please insert value between 1 and 300 characters");
+            RuleFor(x => x.Description).Length(1, 2000).WithMessage("Incorrect value length. Please insert value between 1 and 2000 characters");
+            RuleFor(x => x.ReleaseYear.ToString()).Length(1, 4);
+        }
+    }
+}
---
+++ GameRev/GameRev.ApplicationServices/API/Validators/Users/AddUsersRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using GameRev.ApplicationServices.API.Domain.Requests.Users;
+
+namespace GameRev.ApplicationServices.API.Validators.Users
+{
+    public class AddUsersRequestValidator : AbstractValidator<AddUsersRequest>
+    {
+        public AddUsersRequestValidator()
+        {
+            RuleFor(x => x.Login).Length(1, 50).WithMessage("Login cannot be empty and cannot be longer than 50 characters.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password cannot be empty.");
+            RuleFor(x => x.ConfirmPassword).Equal(x => x.Password).WithMessage("Password and Confirm password must be the same.");
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage("Name cannot be longer than 50 characters.");
+            RuleFor(x => x.Surname).MaximumLength(50).WithMessage("Surname cannot be longer than 50 characters.");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("There must be email address");
+        }
+    }
+}
